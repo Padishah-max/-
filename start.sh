a84ad25b25d2b700b90527f46500ff53bfdc3e49
Padishah-max <mkurgan3@gmail.com>
2025-09-29 23:11:32 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python3 -V || true
-pip3 uninstall -y python-telegram-bot telegram || true
-pip3 install --upgrade --no-cache-dir python-telegram-bot==20.7
-python3 -u main.py
