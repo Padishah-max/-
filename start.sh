fe7893fca47bee66204a61c6309a8a24cfe55e9b
Padishah-max <mkurgan3@gmail.com>
2025-09-30 21:02:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,16 @@
 set -e
 python3 -V || true
 
+# venv
 if [ ! -d ".venv" ]; then
   python3 -m venv .venv
 fi
 source .venv/bin/activate
 python -V
 
+# deps
 python -m pip install --upgrade pip
 python -m pip install --no-cache-dir -r requirements.txt
 
+# run
 exec python -u main.py
